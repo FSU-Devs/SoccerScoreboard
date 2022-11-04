b319004386b5bd7e95b62531b0514825551e7bf3
Raymond Johnson <raymondjohnson@Raymonds-MacBook-Pro.local>
2022-11-04 18:53:21 -0400

    updates

---
+++ Main.js
@@ -0,0 +1,23 @@
+/**
+ * In this file we will store all of the logic that will be 
+ * related to our scoreboard 
+ */
+
+//need to import the scoreboard
+import ScoreboardView from "./scoreboard/ScoreboardView.js";
+
+
+let playerOneScore = 0;
+let playerTwoScore = 0;
+const root = document.querySelector("#app");
+const view = new ScoreboardView(root, "Player One", "Player Two", (player, direction) => {
+	const difference = direction === "minus" ? -1 : 1;
+
+	if (player === "one") {
+		playerOneScore = Math.max(playerOneScore + difference, 0);
+	} else {
+		playerTwoScore = Math.max(playerTwoScore + difference, 0);
+	}
+
+	view.update(playerOneScore, playerTwoScore);
+});
