6861c3938e79dec09ce866133906a36e3b26f5a6
ben-platt23 <86609189+ben-platt23@users.noreply.github.com>
2022-11-30 16:40:54 -0500

    fixed scoreboard to not update with empty values. Also, the score no longer changes on the editing page. This helps with changing the team names without changing the score.

--- Main.js
+++ Main.js
@@ -8,29 +8,32 @@ import ScoreboardView from "./scoreboard/ScoreboardView.js";
 import Timer from "./scoreboard/Timer.js";
 let playerOneScore = 0;
 let playerTwoScore = 0;
-let playerOneName = null;
-let playerTwoName = null;
+let playerOneName = "Team One Name";
+let playerTwoName = "Team Two Name";
 const root = document.querySelector("#app");
-const view = new ScoreboardView(root, "Team one name", "Team two name", (player, direction) => {
+const view = new ScoreboardView(root, playerOneName, playerTwoName, (player, direction) => {
 	const difference = direction === "minus" ? -1 : 1;
 
-	if (player === "one") {
-		playerOneScore = Math.max(playerOneScore + difference, 0);
-	} else {
-		playerTwoScore = Math.max(playerTwoScore + difference, 0);
+	// Doesn't change the name unless you are on the editing page. 
+	// also doesn't change the name unless you enter a non-empty value
+	// finally it doesn't update the score if you are on the editing page
+	if(document.URL.includes("EditingPage.html")){
+		playerOneName = prompt("Please enter the team name for #1");
+		if(playerOneName == null || playerOneName == ""){
+			return;
+		}
+		playerTwoName = prompt("Please enter the team name for #2");
+		if(playerTwoName == null || playerTwoName == ""){
+			return;
+		}
+		view.updateName(playerOneName, playerTwoName)
 	}
-	view.update(playerOneScore, playerTwoScore);
-
-	//I need to figure this part out in order to change the name for both
-	// of the teams name
-	
-		playerOneName = prompt("Please enter the team name: ");
-		if(playerOneName == null){
-			return;
+	else{
+		if (player === "one") {
+			playerOneScore = Math.max(playerOneScore + difference, 0);
+		} else {
+			playerTwoScore = Math.max(playerTwoScore + difference, 0);
 		}
-		playerTwoName = prompt("Please enter the team name: ");
-		if(playerTwoName == null){
-			return;
-		}
-		view.updateName(playerOneName, playerTwoName);
+		view.update(playerOneScore, playerTwoScore);	
+	}
 });
